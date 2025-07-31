d6cd764b2360e76aa6424e7cb0f7f15aafb55404
Testuser <duraiatpeers@gmail.com>
2025-07-31 11:34:25 +0530

    Demo

--- day4/test_demo3_new.py
+++ day4/test_demo3_new.py
@@ -1,45 +1,16 @@
 import openpyxl
 
 import pytest
-
 from openpyxl import load_workbook
 
-def get_data():
 
-   wb = load_workbook("testdata1.xlsx")
-   ws = wb["Data"]  # or wb.active
+def test_demo():
 
-   ws['C1'] = "Status"
-   ws['C2'] = "Pass"
-   ws['C3'] = "Fail"
+    wb = load_workbook("testdata1.xlsx")
+    sheet = wb["Data"]
 
-   wb.save("testdata1.xlsx")
+    print(sheet.title)
+    sheet['C1'] = "Pass"
+    sheet['C2'] = "Fail"
 
-   # Get the first sheet (index 0)
-   sheet = wb.worksheets[0]
-
-   # Print sheet name
-   print(sheet.title)
-
-   # Get the first sheet (index 0)
-   sheet = wb.worksheets[1]
-
-   # Print sheet name
-   print(sheet.title)
-
-
-def test1():
-   get_data()
-   assert False
-
-
-import logging
-
-# log_obj = logging.getLogger()
-# log_format = "%(levelname)s %(msg)s"
-# logging.basicConfig(filename="logreports.txt", filemode="w",format=log_format, level="DEBUG")
-# log_obj.debug('Log message - info')
-# log_obj.info('Log message - info')
-# log_obj.warning('Log message - info')
-# log_obj.error('Log message - info')
-# log_obj.critical('Log message - info')
+    wb.save("testdata1.xlsx")
